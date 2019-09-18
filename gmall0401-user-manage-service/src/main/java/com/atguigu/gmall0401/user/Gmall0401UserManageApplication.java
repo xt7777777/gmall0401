c127820abeb30fb8c5e10583a53f6657276e79ba
xtsky <1143754412@qq.com>
2019-09-18 23:30:03 +0800

    9/17 完成 ！

--- gmall0401-user-manage-service/src/main/java/com/atguigu/gmall0401/user/Gmall0401UserManageApplication.java
+++ gmall0401-user-manage-service/src/main/java/com/atguigu/gmall0401/user/Gmall0401UserManageApplication.java
@@ -2,10 +2,12 @@ package com.atguigu.gmall0401.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
-@MapperScan(basePackages = "com.atguigu.gmall0401.user.mapper")
+@MapperScan(basePackages = "com.atguigu.gmall0401")
+@ComponentScan(basePackages = "com.atguigu.gmall0401")
 public class Gmall0401UserManageApplication {
 
 	public static void main(String[] args) {
